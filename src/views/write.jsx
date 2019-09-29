f8cffd4972a7e660fae85de746d121a98a73f8de
“tangwenjian” <tangwenjian@aspirecn.com>
2019-09-29 17:57:14 +0800

    ...

--- src/views/write.jsx
+++ src/views/write.jsx
@@ -1,35 +1,44 @@
+/* eslint-disable react/no-string-refs */
 import React, {PureComponent} from 'react'
 import request from '../utils/request'
-// import { Button } from 'antd-mobile'
-export default class Page1 extends PureComponent{
+export default class Write extends PureComponent{
     constructor(props){
         super(props)
-        this.state = {
-            data:{}
+    }
+    // 返回底部输入框高度
+    textareaHeight = () => {
+        const H = document.body.scrollHeight
+        const W = document.body.scrollWidth
+        // 顶部翻译框高度
+        const interpretHeight = 100 * 375 / W
+        return H - interpretHeight
+    }
+    change(){
+        // console.log(this.refs.textarea.value)
+        const val = String(this.refs.textarea.value)
+        if(/\B$/.test(val)){
+            const chinese = val.match(/\B[\W(?!,)+](?!\n)$/)
+            if(chinese){
+                console.log('fanyi')
+                console.log(chinese,'chinese')
+            }
+            
+            let baseUrl = 'http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i='
+            request(baseUrl+chinese)
         }
     }
-    click=()=>{
-        request('api/page1').then(data=>{
-            this.setState({
-                data: data
-            })
-        })
+      
+    componentDidMount(){
+        document.querySelector('textarea').style.height = this.textareaHeight() + 'px'
+        this.refs.textarea.value=''
     }
-    // eslint-disable-next-line no-undef
-    // artEditor = new Eleditor({
-    //     el: '#article-body',
-    //     upload:{
-    //         server: '/upload.php',
-    //         fileSizeLimit: 2
-    //     }
-    // })
     render(){
         return (
             <div className="write">
                 <div className="interpret">
                 </div>
                 <div className="input">
-                    <input type="text"/>
+                    <textarea ref='textarea' cols="30" rows="10"  onChange={this.change.bind(this)} ></textarea>
                 </div>
             </div>
         )
